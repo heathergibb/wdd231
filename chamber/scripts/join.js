fdecd0a1cae000301164e122f82ec0862ab944bf
heathergibb <hrgibb@gmail.com>
2024-07-05 13:55:45 -0600

    ready for testing

---
+++ chamber/scripts/join.js
@@ -0,0 +1,23 @@
+const modalLink = document.querySelector("#membership-level-link");
+const membershipDialog = document.querySelector("#membership-dialog");
+
+modalLink.addEventListener("click", () => {
+    membershipDialog.showModal();
+})
+
+const closeModal = document.querySelector("#close-modal");
+
+closeModal.addEventListener("click", () => {
+    membershipDialog.close();
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    const timestamp = document.querySelector("#timestamp");
+
+    timestamp.value = Date().toString();
+    console.log(timestamp.value);
+})
+
+
+
+
